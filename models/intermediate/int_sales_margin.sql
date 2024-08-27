38035efbc77d9ad3d128332b68511b154b46df76
Miller <cinticonadiaz@gmail.com>
2024-08-27 22:20:08 +0000

    Calculate the Margin per Sale(margin, purchase_cost)

---
+++ models/intermediate/int_sales_margin.sql
@@ -0,0 +1,21 @@
+WITH sales_data AS (
+    SELECT 
+        s.orders_id,
+        s.quantity,
+        s.revenue,
+        p.products_id,
+        p.purchase_price  -- Usa el nombre de columna correcta para el precio de compra
+    FROM 
+        {{ ref('stg_raw__sales') }} s  -- Refiere el modelo de ventas
+    JOIN 
+        {{ ref('stg_raw__product') }} p  -- Refiere el modelo de productos
+    ON 
+        s.products_id = p.products_id  -- Une las tablas usando el id del producto
+)
+
+SELECT 
+    sales_data.*,
+    (ROUND(sales_data.quantity * CAST(sales_data.purchase_price AS FLOAT64),2)) AS purchase_cost,  -- Calcular purchase_cost
+    (ROUND(sales_data.revenue - (sales_data.quantity * CAST(sales_data.purchase_price AS FLOAT64)),2)) AS margin  -- Calcular margin
+FROM 
+    sales_data
