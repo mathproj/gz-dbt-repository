84d2db82ebe5fa00dfb4c9dad453a0852bbe9a0a
Miller <cinticonadiaz@gmail.com>
2024-08-27 22:40:33 +0000

    Calculate the Margin per Order

--- models/intermediate/int_orders_margin.sql
+++ models/intermediate/int_orders_margin.sql
@@ -2,14 +2,15 @@ WITH order_data AS (
     SELECT
         orders_id,
         date_date,
-        SUM(revenue) AS revenue,  -- Suma el revenue por order_id
-        SUM(quantity) AS quantity,  -- Suma la cantidad por order_id
-        SUM(purchase_cost) AS purchase_cost,  -- Suma el costo de compra por order_id
-        SUM(margin) AS margin  -- Suma el margen por order_id
+        ROUND(SUM(revenue),2) AS revenue,  -- Suma el revenue por order_id
+        ROUND(SUM(quantity),2) AS quantity,  -- Suma la cantidad por order_id
+        ROUND(SUM(purchase_cost),2) AS purchase_cost,  -- Suma el costo de compra por order_id
+        ROUND(SUM(margin),2) AS margin  -- Suma el margen por order_id
     FROM
         {{ ref('int_sales_margin') }}  -- Referencia el modelo anterior
     GROUP BY
         orders_id, date_date  -- Agrupa por order_id y fecha
+    ORDER BY orders_id DESC, date_date DESC
 )
 
 SELECT
