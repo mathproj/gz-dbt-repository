e238e3af570b97dda85a8e576a450a14423560be
cindy ticona <cinticonadiaz@gmail.com>
2024-08-28 03:02:38 +0000

    cambios

--- models/intermediate/int_orders_margin.sql
+++ models/intermediate/int_orders_margin.sql
@@ -1,24 +1,10 @@
-WITH order_data AS (
-    SELECT
-        orders_id,
-        date_date,
-        ROUND(SUM(revenue),2) AS revenue,  -- Suma el revenue por order_id
-        ROUND(SUM(quantity),2) AS quantity,  -- Suma la cantidad por order_id
-        ROUND(SUM(purchase_cost),2) AS purchase_cost,  -- Suma el costo de compra por order_id
-        ROUND(SUM(margin),2) AS margin  -- Suma el margen por order_id
-    FROM
-        {{ ref('int_sales_margin') }}  -- Referencia el modelo anterior
-    GROUP BY
-        orders_id, date_date  -- Agrupa por order_id y fecha
-    ORDER BY orders_id DESC, date_date DESC
-)
-
 SELECT
-    orders_id,
-    date_date,
-    revenue,
-    quantity,
-    purchase_cost,
-    margin
-FROM
-    order_data
+  orders_id,
+  max(date_date) as date_date,
+  ROUND(SUM(revenue),2) as revenue,
+  ROUND(SUM(quantity),2) as quantity,
+  ROUND(SUM(purchase_cost),2) as purchase_cost,
+  ROUND(SUM(margin),2) as margin
+FROM {{ ref("int_sales_margin") }}
+GROUP BY orders_id
+ORDER BY orders_id DESC
--- models/intermediate/int_sales_margin.sql
+++ models/intermediate/int_sales_margin.sql
@@ -1,22 +1,12 @@
-WITH sales_data AS (
-    SELECT 
-        s.orders_id,
-        s.date_date,
-        s.quantity,
-        s.revenue,
-        p.products_id,
-        p.purchase_price  -- Usa el nombre de columna correcta para el precio de compra
-    FROM 
-        {{ ref('stg_raw__sales') }} s  -- Refiere el modelo de ventas
-    JOIN 
-        {{ ref('stg_raw__product') }} p  -- Refiere el modelo de productos
-    ON 
-        s.products_id = p.products_id  -- Une las tablas usando el id del producto
-)
-
 SELECT 
-    sales_data.*,
-    (ROUND(sales_data.quantity * CAST(sales_data.purchase_price AS FLOAT64),2)) AS purchase_cost,  -- Calcular purchase_cost
-    (ROUND(sales_data.revenue - (sales_data.quantity * CAST(sales_data.purchase_price AS FLOAT64)),2)) AS margin  -- Calcular margin
-FROM 
-    sales_data
+    products_id, 
+    date_date, 
+    orders_id,
+    revenue, 
+    quantity, 
+    CAST(purchase_price AS FLOAT64), 
+    ROUND(s.quantity*CAST(p.purchase_price AS FLOAT64),2) AS purchase_cost,
+    s.revenue - ROUND(s.quantity*CAST(p.purchase_price AS FLOAT64),2) AS margin
+FROM {{ref("stg_raw__sales")}} s
+LEFT JOIN {{ref("stg_raw__product")}} p 
+    USING (products_id)
